dd32eeed2fa31504513155eb15c6f27383609d3b
kijiwon <kiji1313@gmail.com>
2024-09-11 00:21:10 +0900

    time api router

--- src/pages/api/time.ts
+++ src/pages/api/time.ts
@@ -3,5 +3,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const date = new Date();
-  res.json({ time: date.toLocaleString() });
+  res.status(200).json({ time: date.toLocaleString() });
 }
