1e04a7f3c321563466f8da53713a2f3f01adecee
kijiwon <kiji1313@gmail.com>
2024-09-12 00:13:20 +0900

    searchbar styling

--- src/components/searchable-layout.tsx
+++ src/components/searchable-layout.tsx
@@ -1,16 +1,51 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useState } from "react";
 import { ReactNode } from "react";
+import style from "./searchable-layout.module.css";
 
 export default function SearchableLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const q = router.query.q as string;
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    // 브라우저 새로고침 시 검색창 유지
+    if (!search || q === search) return; // 동일한 검색어 입력 시 페이지 이동 안함
+    router.push(`/search?q=${search}`);
+  };
+
+  // enter키 누르면 검색
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
   return (
     <div>
-      <div>
-        <div>서치바</div>
-        {children}
+      <div className={style.searchbar_container}>
+        <input
+          value={search}
+          onKeyDown={onKeyDown}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요 ..."
+        />
+        <button onClick={onSubmit}>검색</button>
       </div>
+      {children}
     </div>
   );
 }
