bdf592ace373791be9294045e40ed93e30e864f6
kijiwon <kiji1313@gmail.com>
2024-09-14 22:30:37 +0900

    SSG - 동적 경로

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,10 +1,21 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+// 존재할 수 있는 경로를 리턴
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } }, // url parameter의 값은 문자열로 명시
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    // paths에 명시되지 않은 경로로 접근할 경우 대체 설정
+    fallback: false, // 존재하지 않는 경로의 요청은 Not Found 처리 
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id; // !단언을 사용해 params가 존재함을 단언 <- 이 페이지는 url 파라미터가 있어야 접근할 수 있기 때문
   const book = await fetchOneBook(Number(id));
   return {
@@ -14,7 +25,7 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   // null 처리
   if (!book) return "도서의 정보를 불러올 수 없습니다.";
 
