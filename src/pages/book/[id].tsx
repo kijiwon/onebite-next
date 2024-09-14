a4631545e31e9e2270265016599d1acdc6a4e4be
kijiwon <kiji1313@gmail.com>
2024-09-14 23:34:23 +0900

    SSG - 렌더링 옵션 추가

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
 
 // 존재할 수 있는 경로를 리턴
 export const getStaticPaths = () => {
@@ -20,6 +21,14 @@ export const getStaticPaths = () => {
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id; // !단언을 사용해 params가 존재함을 단언 <- 이 페이지는 url 파라미터가 있어야 접근할 수 있기 때문
   const book = await fetchOneBook(Number(id));
+
+  // 존재하지 않는 도서인 경우 not found 페이지로 보내기
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: { book },
   };
@@ -28,6 +37,9 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  // fallback 상태 처리
+  if (router.isFallback) return "로딩 중 입니다.";
   // null 처리
   if (!book) return "도서의 정보를 불러올 수 없습니다.";
 
