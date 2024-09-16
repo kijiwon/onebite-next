f083e87730e36a69d48d25d082b31024b6f2b7b2
kijiwon <kiji1313@gmail.com>
2024-09-16 18:52:13 +0900

    vercel 배포 및 서버 주소 변경

--- src/lib/fetch-books.ts
+++ src/lib/fetch-books.ts
@@ -1,7 +1,7 @@
 import { BookData } from "@/types";
 
 export default async function fetchBooks(q?: string): Promise<BookData[]> {
-  let url = `http://localhost:12345/book`;
+  let url = `https://onebite-books-server-main-rho.vercel.app/book`;
 
   // 검색 결과를 보내는 경우
   if (q) {
