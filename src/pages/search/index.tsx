7d11e5ca2115e5bf89368d12b626c2d8f2925d91
kijiwon <kiji1313@gmail.com>
2024-09-16 18:25:40 +0900

    SEO - search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useState } from "react";
 import { BookData } from "@/types";
+import Head from "next/head";
 
 // export const getServerSideProps = async (
 //   context: GetServerSidePropsContext
@@ -40,6 +41,15 @@ export default function Page() {
 
   return (
     <div>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스 - 검색결과" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
       {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
